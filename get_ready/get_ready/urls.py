a45e64576938d8d063e1fe08f197304aa8296b8b
Dim_Aks <kiton444@gmail.com>
2025-03-04 20:43:06 +0300

    Созданы: админ панель и суперпользователь, создана таблица в бд для хранения данных по встречам

--- get_ready/get_ready/urls.py
+++ get_ready/get_ready/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('meetings/', include('meetings.urls')),
+    path('', include('meetings.urls')),
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "Панель решалы"
--- get_ready/meetings/views.py
+++ get_ready/meetings/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+
+#from get_ready.meetings.forms import MeetingForm
+#from get_ready.meetings.models import Meeting
+
 
 def index(requests):
     return render(requests, 'meetings/index.html')
+
+def create_suggest(requests):
+    # error = ''
+    # if requests.method == 'POST':
+    #     form = MeetingForm(requests.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('home')
+    #     else:
+    #         error = 'Форма былa заполнена неверно'
+    #
+    # form = MeetingForm()
+    # data ={'form': form,
+    #        'error': error}
+    return render(requests, 'meetings/suggest_an_appointment.html')
+
+def check(requests):
+    return render(requests, 'meetings/check.html')
