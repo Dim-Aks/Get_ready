baedefb9f6879b118508799fce1408b557fe2905
Dim-Aks <kiton444@gmail.com>
2025-03-07 21:28:56 +0300

    Delete get_ready directory

--- get_ready/meetings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('suggest/', views.create_suggest, name='suggest'),
-    path('check/', views.check, name='check'),
-]
