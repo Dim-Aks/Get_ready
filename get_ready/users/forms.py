8334453b97ac76ee9069cf7331504d718dbe6d12
Dim_Aks <kiton444@gmail.com>
2025-03-13 19:14:47 +0300

    Сделана форма регистрации пользователей с проверкой на уникальность логина и корректность пароля

---
+++ get_ready/users/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+ # Форма регистрации
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class':'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class':'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class':'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            'email': 'E-mail',
+        }
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-input'}),
+        }
+    
+    # Проверка пароля на уникальность
+    def clean_email(self):
+        User = get_user_model()
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Такой E-mail уже существует!")
+        return email
