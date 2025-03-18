0128d7113b208d6f0b4e6996e27630d6b034f489
Dim_Aks <kiton444@gmail.com>
2025-03-18 18:36:23 +0300

    paginations_check.html

--- get_ready/meetings/views.py
+++ get_ready/meetings/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import MeetingForm, CommentForm
 from .models import Meeting
@@ -8,9 +9,9 @@ def create_suggest(request):
     if request.method == 'POST':
         form = MeetingForm(request.POST)
         if form.is_valid():
-            meeting = form.save(commit=False) # Сохраняем форму без коммита в БД
+            meeting = form.save(commit=False) # Сохраняем форму без комита в БД
             meeting.author = request.user # Присваиваем автора
-            meeting.save()  # Теперь сохраняем в БД
+            meeting.save()  # сохраняем в БД
             return redirect('check') # Перенаправляем на страницу с созданными встречами
     else:
         form = MeetingForm()
@@ -20,8 +21,11 @@ def create_suggest(request):
 
 @login_required
 def view_meetings(request):
-    meetings = Meeting.objects.all().order_by('date_meeting')
-    return render(request, 'meetings/check.html', {'meetings': meetings})
+    meetings = Meeting.objects.all().order_by('date_meeting') # все встречи из базы
+    paginator = Paginator(meetings, 4)  # пагинатор по 4 элемента на страницу
+    page_number = request.GET.get('page', 1)  # номер страницы из GET-параметра
+    page_obj = paginator.get_page(page_number)   # объект страницы
+    return render(request, 'meetings/check.html', {'page_obj': page_obj})
 
 def meeting_detail(request, pk):
     meeting = get_object_or_404(Meeting, pk=pk)
