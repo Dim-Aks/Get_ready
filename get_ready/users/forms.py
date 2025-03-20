8e7d4c08e3138815571295452a0f3407e267fa31
Dim_Aks <kiton444@gmail.com>
2025-03-20 20:20:42 +0300

    Добавлена кнопка Регистрация на странице входа

--- get_ready/users/forms.py
+++ get_ready/users/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
  # Форма регистрации
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class':'form-input'}))
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class':'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class':'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class':'form-input'}))
 
--- get_ready/users/urls.py
+++ get_ready/users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import RegisterUser, logout_user, LoginUser
-from django.views.generic.base import TemplateView
 
 app_name = "users"
 
