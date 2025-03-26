3ea62338d065d6301f9e931e875190bdb79dc374
Dim_Aks <kiton444@gmail.com>
2025-03-26 14:14:42 +0300

    delete_meeting_page

--- get_ready/meetings/urls.py
+++ get_ready/meetings/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
 from django.views.generic.base import TemplateView
-
+from .views import UpdateMeeting, meeting_detail, view_meetings, MeetingCreateView, DeleteMeeting
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='meetings/index.html', extra_context={'title': "Собирайся!"}), name='home'),
-    path('suggest/', views.create_suggest, name='suggest'),
-    path('check/', views.view_meetings, name='check'),
-    path('check/<int:pk>/', views.meeting_detail, name='meeting_detail'),
-    path('edit/<int:pk>/', views.UpdateMeeting.as_view(), name='edit_page'),
+    path('suggest/', MeetingCreateView.as_view(), name='suggest'),
+    path('check/', view_meetings, name='check'),
+    path('check/<int:pk>/', meeting_detail, name='meeting_detail'),
+    path('edit/<int:pk>/', UpdateMeeting.as_view(), name='edit_page'),
+    path('check/<int:pk>/delete', DeleteMeeting.as_view(), name='meeting_delete')
 ]
