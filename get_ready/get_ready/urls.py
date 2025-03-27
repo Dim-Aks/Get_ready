daeb2317bea19e610e791181f62786cf0571bdb1
Dim_Aks <kiton444@gmail.com>
2025-03-27 13:18:11 +0300

    favicon

--- get_ready/get_ready/urls.py
+++ get_ready/get_ready/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,6 +7,6 @@ urlpatterns = [
     path('', include('meetings.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace="users")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header = "Панель решалы"
