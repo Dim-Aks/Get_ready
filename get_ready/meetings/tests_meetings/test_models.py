fba3be8249f0e36f0bfe48bfd5276da6f7a67f6e
Dim_Aks <kiton444@gmail.com>
2025-04-08 13:31:04 +0300

    добавлена фикстура создания user and meeting

--- get_ready/meetings/tests_meetings/test_models.py
+++ get_ready/meetings/tests_meetings/test_models.py
@@ -4,11 +4,15 @@ import pytest
 from django.contrib.auth.models import User
 from ..models import Comment, Meeting
 
+date_meet = datetime.now()
 
-@pytest.mark.django_db
-def test_meeting_creation():
+@pytest.fixture
+def user():
     user = User.objects.create(username='testuser', password='password')
-    date_meet = datetime.now()
+    return user
+
+@pytest.fixture
+def meeting(user):
     meeting = Meeting.objects.create(author=user,
                                      reason_to_meet='Test Meeting',
                                      meeting_place='Test Meeting',
@@ -17,6 +21,11 @@ def test_meeting_creation():
                                      dress_code='Test Meeting',
                                      date_meeting=date_meet,
                                      )
+    return meeting
+
+@pytest.mark.django_db
+def test_meeting_creation(user, meeting):
+
     assert Meeting.objects.count() == 1
     assert meeting.author == user
     assert meeting.reason_to_meet =='Test Meeting'
@@ -28,17 +37,9 @@ def test_meeting_creation():
 
 
 @pytest.mark.django_db
-def test_comment_creation():
-    user = User.objects.create(username='testuser', password='password')
-    meeting = Meeting.objects.create(author=user,
-                                     reason_to_meet='Test Meeting',
-                                     meeting_place = 'Test Meeting',
-                                     address = 'Test Meeting',
-                                     what_to_do = 'Test Meeting',
-                                     dress_code = 'Test Meeting',
-                                     date_meeting = datetime.now(),
-                                     )
+def test_comment_creation(user, meeting):
     comment = Comment.objects.create(meeting=meeting, author=user, text='This is a test comment')
+
     assert Comment.objects.count() == 1
     assert comment.meeting == meeting
     assert comment.author == user
