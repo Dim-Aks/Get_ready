a49be0c6a476c29215bcb8fc44fede202afe4d9e
Dim_Aks <kiton444@gmail.com>
2025-04-10 19:23:25 +0300

    тест формы пользователя ЛК, готова часть тестов на форму регистрации

--- get_ready/users/tests_users/test_users_forms.py
+++ get_ready/users/tests_users/test_users_forms.py
@@ -1,7 +1,15 @@
 import pytest
 from django import forms
 from django.contrib.auth.models import User
-from ..forms import ProfileUserForm
+from ..forms import ProfileUserForm, RegisterUserForm, UserPasswordChangeForm
+
+
+# тестовый пользователь
+# @pytest.fixture()
+# def user():
+#     return User.objects.create(
+#         username='Test_user',
+#         password='Password')
 
 
 # тестирование формы профиля пользователя в ЛК
@@ -29,7 +37,62 @@ class TestProfileUserForm:
         assert email_form.widget.attrs['class'] == 'form-input'
 
     # тест на корректность данных в форме
+    @pytest.mark.django_db
+    def test_form_data(self):
+        t_user = User.objects.create(username='testuser', email='test@test.com')
+        form = ProfileUserForm(instance=t_user)
+        assert form.initial['username'] == 'testuser'
+        assert form.initial['email'] == 'test@test.com'
+
+    # тест на неизменяемость полей
+    @pytest.mark.django_db
+    def test_disable_form(self):
+
+        form = ProfileUserForm(data = {
+            'username': 'new_user',
+            'email': 'new@test.com'
+        })
+        assert form.is_valid() is False
+        assert 'username' in form.errors
+        assert 'email' in form.errors
+
+# тестирование формы регистрации пользователя
+class TestRegisterUserForm:
+
+    # тест класса мета
+    def test_meta_data(self):
+        assert RegisterUserForm.Meta.model == User
+        assert RegisterUserForm.Meta.fields == ['username', 'email', 'password1', 'password2']
+        assert RegisterUserForm.Meta.labels == {'email': 'E-mail', }
+        assert RegisterUserForm.Meta.widgets['email'].attrs['class'] == 'form-input'
+
+    # тест полей формы
+    def test_form_field(self):
+
+        # поле username
+        username_form = RegisterUserForm.base_fields['username']
+        assert username_form.label == 'Имя пользователя'
+        assert isinstance(username_form.widget, forms.TextInput)
+        assert username_form.widget.attrs['class'] == 'form-input'
+
+        # поле password1
+        password1_form = RegisterUserForm.base_fields['password1']
+        assert password1_form.label == 'Пароль'
+        assert isinstance(password1_form.widget, forms.PasswordInput)
+        assert password1_form.widget.attrs['class'] == 'form-input'
+
+        # поле password2
+        password2_form = RegisterUserForm.base_fields['password2']
+        assert password2_form.label == 'Повтор пароля'
+        assert isinstance(password2_form.widget, forms.PasswordInput)
+        assert password2_form.widget.attrs['class'] == 'form-input'
+
+    # тест
     # @pytest.mark.django_db
-    # def test_form_data(self):
-    #     pass
-        # assert ProfileUserForm['username'] == 'testuser'
+    # def test_clean_email(self):
+    #     data = {'username':'Test_user',
+    #             'email':'Test@test.com',
+    #             'password1':'Test_password1',
+    #             'password2':'Test_password2'}
+    #     form_uniq = RegisterUserForm(data=data)
+    #     assert form_uniq.is_valid() is False
