8a7f973c094aeddf727f83fa04abe4acd204158e
Dim-Aks <kiton444@gmail.com>
2025-04-25 17:29:44 +0300

    db lite3

--- get_ready/get_ready/settings.py
+++ get_ready/get_ready/settings.py
@@ -87,8 +87,6 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
     }
 }
 
