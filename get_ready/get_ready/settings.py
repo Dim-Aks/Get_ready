1a6a782e85ca30bee493c7f1f595fd41336f16a5
Dim-Aks <kiton444@gmail.com>
2025-04-28 13:12:37 +0300

    кнопки пагинации только когда страниц больше чем 1

--- get_ready/get_ready/settings.py
+++ get_ready/get_ready/settings.py
@@ -21,17 +21,13 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['get-ready-3rb0.onrender.com']
+ALLOWED_HOSTS = ['get-ready-3rb0.onrender.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -88,6 +84,7 @@ DATABASES = {
     }
 }
 
+# postgresql
 # DATABASES = {
 #     "default": dj_database_url.config(
 #         default=os.getenv('DATABASE_URL')
@@ -138,7 +135,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Абсолютный путь для сбора статики
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] # Общая статика проекта
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # добавляет хэши к именам файлов (cache-busting) и сжимает файлы gzip
+# добавляет хэши к именам файлов (cache-busting) и сжимает файлы gzip
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
